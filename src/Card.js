e8949797407eb8f96cce5ecea6170c7393d3e51f
Chester <chesterdeveloper2000@gmail.com>
2020-08-06 17:38:25 +0800

    deleted stuffs that aren't needed, created a new component called Card then appending it

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/test?200x200" alt="robots"/>
+            <div>
+                <h2>John Doe</h2>
+                <p>john.doe@email.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
