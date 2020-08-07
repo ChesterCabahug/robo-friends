cc5a8587f466ce6c922f4474dd4f6ce695bff858
Chester <chesterdeveloper2000@gmail.com>
2020-08-07 14:39:30 +0800

    making code cleaner

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import CardList from './CardList';
-import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import "tachyons"
 import { robots } from "./robots"
