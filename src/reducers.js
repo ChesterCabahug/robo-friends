4770da851aa2c47e5ff55705f29bac36fc81cd04
Chester <chesterdeveloper2000@gmail.com>
2020-08-08 17:09:44 +0800

    working on the 'store' thingy

--- src/reducers.js
+++ src/reducers.js
@@ -1,18 +1,17 @@
-import { CHANGE_SEARCH_FIELD } from "./constants.js"
-
+import { CHANGE_SEARCH_FIELD } from "./constants.js";
 
 const initialState = {
-    searchField: ""
-}
+    searchField: "",
+};
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state = initialState, action = {}) => {
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, {searchField: action.payload})
+            return Object.assign({}, state, { searchField: action.payload });
             break;
-    
+
         default:
-            return state
+            return state;
             break;
     }
-}
+};
